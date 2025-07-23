d30f49ce2f9c8c68d5b332416264cc74d57b9277
Aziko050050 <aziko050050@gmail.com>
2025-07-24 00:40:42 +0600

    loginTest Feature

--- src/main/java/org/example/page/DashboardPlusPage.java
+++ src/main/java/org/example/page/DashboardPlusPage.java
@@ -1,4 +1,20 @@
 package org.example.page;
 
-public class DashboardPlusPage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPlusPage extends BasePage{
+
+    @FindBy(xpath = "//span[@class = 'arrow-down']")
+    public WebElement arrowDownElement;
+
+    @FindBy(xpath = "//div[@class = 'menu-page-item']//p[text() = 'Go to legacy interface']")
+    public WebElement legacyPageElement;
+
+    public DashboardPlusPage dashboardPlusPage() {
+        action.clickBtn(arrowDownElement).clickBtn(legacyPageElement);
+        return this;
+    }
+
+
 }
